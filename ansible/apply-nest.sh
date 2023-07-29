ac1a60c201ebd1855fb96cefe2abb69389972e18
Buba <bubacoder@gmail.com>
2023-07-29 18:16:38 +0200

    Convert Ansible inventory to yaml, add config for nas and proxmox

--- ansible/apply-nest.sh
+++ ansible/apply-nest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # If passwordless sudo is not enabled on the hosts, need to execute with the "--ask-become-pass" parameter
+# Dry-run with "--check --diff" or short: "-CD"
 
-ansible-playbook ./nest.yaml "$@"
+ansible-playbook ./nest.yaml --limit docker_hosts "$@"
