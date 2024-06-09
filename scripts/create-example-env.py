4ace953912cfc5fd3b83c27633c363d78b77a1f5
Buba <bubacoder@gmail.com>
2024-06-09 19:09:37 +0000

    Add example .env generator script

---
+++ scripts/create-example-env.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import sys
+
+
+SENSITIVE_VARS_TO_MASK = [
+    'KEY',
+    'PASSWORD',
+    'TOKEN',
+    'SECRET',
+    'SENSITIVE',
+]
+
+SENSITIVE_VARS_TO_GENERALIZE = {
+    'TIMEZONE': 'Etc/UTC',
+    'MYDOMAIN': 'example.com',
+    'LOCATION_CITY': 'Greenwich',
+    'LOCATION_LATITUDE': '51.48',
+    'LOCATION_LONGITUDE': '0.00',
+    'ADMIN_USER': 'admin',
+    'ADMIN_EMAIL': 'root@localhost',
+    'ADMIN_DISPLAYNAME': 'AdminUser'
+}
+
+
+def contains_any_substring(string: str, substrings: list[str]) -> bool:
+    for substring in substrings:
+        if substring in string:
+            return True
+    return False
+
+
+def mask_sensitive_variables(input_file: str) -> str:
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+
+    output_lines = []
+    for line in lines:
+        if '=' in line:
+            variable, value = line.strip().split('=', 1)
+            if variable in SENSITIVE_VARS_TO_GENERALIZE:
+                output_lines.append(f"{variable}={SENSITIVE_VARS_TO_GENERALIZE[variable]}")
+            elif contains_any_substring(variable, SENSITIVE_VARS_TO_MASK):
+                output_lines.append(f"{variable}=\"use-some-very-secure-value-here\"")
+            else:
+                output_lines.append(line.strip())
+        else:
+            output_lines.append(line.strip())
+
+    return '\n'.join(output_lines)
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: ./create-example-env.py <input_env_file>")
+        return
+
+    input_file = sys.argv[1]
+    output = mask_sensitive_variables(input_file)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
