f4ea07c4ecc37e94f292ef1633af3214b0033fa3
Buba <bubacoder@gmail.com>
2025-04-13 15:27:47 +0000

    Remove deprecated azurerm config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,7 +15,6 @@ provider "azurerm" {
   features {
     virtual_machine {
       delete_os_disk_on_deletion     = true
-      graceful_shutdown              = false
       skip_shutdown_and_force_delete = false
     }
   }
