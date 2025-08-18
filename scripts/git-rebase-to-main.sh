e2aa47dde6832edabcaf7c9e16b636fee28450b0
Buba <bubacoder@gmail.com>
2025-08-18 20:30:35 +0200

    Use `git rebase --autosquash` command instead of my fixup script
    https://git-scm.com/docs/git-rebase#Documentation/git-rebase.txt---autosquash

--- scripts/git-rebase-to-main.sh
+++ scripts/git-rebase-to-main.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 BASE_REF="$(git symbolic-ref --quiet --short refs/remotes/origin/HEAD 2>/dev/null || echo 'origin/main')"
 STASH_NAME="Automatic-stash-before-rebasing-$(date +%s)"
 
@@ -12,7 +11,7 @@ else
 fi
 
 # Would be possible to use "--autostash", but "--include-untracked" is not supported
-GIT_SEQUENCE_EDITOR="${SCRIPT_DIR}/git-reorder-fixup.py" git rebase --interactive "${BASE_REF}"
+git rebase --interactive --autosquash "${BASE_REF}"
 
 # Not possible to reference stash by name, so the first stash will be popped if the name matches
 if [ "$STASH_SUCCEEDED" = true ] && git stash list --format=%gs | head -1 | grep -q "${STASH_NAME}"; then
