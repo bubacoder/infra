5ba53c549c1c3d2661b5b002697d32dafec4b1e0
Buba <bubacoder@gmail.com>
2025-08-24 17:35:02 +0000

    Fix git rebase script

--- scripts/git-rebase-to-main.sh
+++ scripts/git-rebase-to-main.sh
@@ -5,15 +5,18 @@ BASE_REF="$(git symbolic-ref --quiet --short refs/remotes/origin/HEAD 2>/dev/nul
 STASH_NAME="Automatic-stash-before-rebasing-$(date +%s)"
 
 if git stash push --include-untracked -m "${STASH_NAME}"; then
-    STASH_SUCCEEDED=true
+  STASH_SUCCEEDED=true
 else
-    STASH_SUCCEEDED=false
+  STASH_SUCCEEDED=false
 fi
 
 # Would be possible to use "--autostash", but "--include-untracked" is not supported
 git rebase --interactive --autosquash "${BASE_REF}"
 
-# Not possible to reference stash by name, so the first stash will be popped if the name matches
-if [ "$STASH_SUCCEEDED" = true ] && git stash list --format=%gs | head -1 | grep -q "${STASH_NAME}"; then
+if [ "$STASH_SUCCEEDED" = true ]; then
+  # Not possible to reference stash by name; check if the top stash subject contains the stash name
+  TOP_STASH_SUBJECT="$(git stash list --max-count=1 --format=%gs || true)"
+  if [[ "$TOP_STASH_SUBJECT" == *"$STASH_NAME"* ]]; then
     git stash pop --quiet
+  fi
 fi
