fb3e949e6fa8f3cac59c1fbf4e2e62a9ba27b26e
Buba <bubacoder@gmail.com>
2025-10-06 13:26:18 +0000

    Fix Ansible-PyYAML package incompatibility

--- ansible/bootstrap-ansible.sh
+++ ansible/bootstrap-ansible.sh
@@ -56,9 +56,11 @@ install_ansible_on_debian() {
 
   wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/ansible.list
-  apt-get update && apt-get install --yes ansible python3-pip
+  apt-get update
+  apt-get install --yes ansible python3-pip
 
-  pip install passlib ansible-lint --break-system-packages
+  # "--ignore-installed" is added to fix error "Cannot uninstall PyYAML 6.0.2 ... The package was installed by debian."
+  pip install passlib ansible-lint --ignore-installed --break-system-packages
   ansible-galaxy install -r "${ANSIBLE_DIR}/requirements.yml"
 }
 
